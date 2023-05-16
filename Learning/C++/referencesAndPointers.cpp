5b45c1587120f87ce453645bc60d6ef51f195af5
Pranava Ponvinayagan <67753240+PranavaKP@users.noreply.github.com>
2023-05-16 16:05:47 +0000

    pointers

--- Learning/C++/referencesAndPointers.cpp
+++ Learning/C++/referencesAndPointers.cpp
@@ -13,6 +13,19 @@ int main(){
     string hello = "Bonjour";
     cout << &hello; // outputs the memory address of the variable hello in hexadecimal
 
-    
+    //pointer: a variable that stores the memory address as its value, created with the * opperator after the variable type: the pointer and main variable have to be the same type
+    //example:
+    string food = "Pizza";
+    string* ptr = &food;
+    cout << food; //outputs "Pizza"
+    cout << &food; //outputs memory address of food
+    cout << ptr; //outputs the memory address of food with the pointer
+
+    //deference: output the value of main variable with the pointer: * is used before the pointer variable
+    string candy = "Chocolate";
+    string* ptr = &candy;
+    cout << ptr; //reference: outputs memory address of candy
+    cout << *ptr; //deference: outputs the value of candy
+
 
 }
