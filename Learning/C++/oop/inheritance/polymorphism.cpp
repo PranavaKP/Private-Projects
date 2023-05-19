4d2e191b42e2e9bf61e35d0660c632b959c6339a
Pranava Ponvinayagan <67753240+PranavaKP@users.noreply.github.com>
2023-05-19 15:03:53 +0000

    OPP Finished

--- Learning/C++/oop/inheritance/polymorphism.cpp
+++ Learning/C++/oop/inheritance/polymorphism.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 //polymorphism: uses methods/attributes from base class to perform similar but different tasks, doesn't only inherit them but changes its value
 
-class animals{
+class livingOrganisms{
     public:
         string diet;
         void sound(){
@@ -14,10 +14,43 @@ class animals{
 
 };
 
-class birds : public animals{
+class birds : public livingOrganisms{
     public:
+        string type;
+        string featherColors;
         string diet = "Omnivorous";
         void sound(){
             cout << "Chirp Chirp!";
         }
+        birds(string x,string y){
+            type = x;
+            featherColors = y;
+        }
 };
+
+class plants : public livingOrganisms{
+    public:
+        string type;
+        string flowerColor;
+        string diet = "autotrophic";
+        void sound(){
+            cout << "Doesn't make any noise";
+        }
+        plants(string x, string y){
+            type = x;
+            flowerColor = y;
+        }
+};
+
+int main(){
+    
+    birds Parrot("Parrot", "Green");
+    cout << Parrot.diet << " " + Parrot.featherColors << " " + Parrot.type;
+    Parrot.sound();
+
+    plants AppleTree("Tree", "Red");
+    cout << AppleTree.diet << " " + AppleTree.flowerColor << " " + AppleTree.type;
+    AppleTree.sound();
+
+    return 0;
+}
