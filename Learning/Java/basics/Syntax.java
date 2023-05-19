430465537730b95dff69328c88754ed06ca65b68
Pranava Ponvinayagan <67753240+PranavaKP@users.noreply.github.com>
2023-05-19 16:53:35 +0000

    start of java

---
+++ Learning/Java/basics/Syntax.java
@@ -0,0 +1,20 @@
+
+//when creating a file a public class with the name of the file must also be created
+//inside the class, a method with the name of the file must also be created, anyting in that method will be executed when the file is run
+
+public class Syntax { //class
+    public static void syntax(String[] args) { //method
+        //print with a new line
+        System.out.println("Hello");
+
+        //print without a new line
+        System.out.print("Hello");
+
+        //variables
+            string exampleString = "Hello";
+            int exampleInt = 1;
+            float exampleFloat = 1.1;
+            char exampleChar = 'A';
+            boolean = true; 
+    }
+}
