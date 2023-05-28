a620d34e1b9da39ae2f19f83a6bf7ab158179c8f
Pranava Ponvinayagan <67753240+PranavaKP@users.noreply.github.com>
2023-05-28 01:44:17 +0000

    gameplay and refinement of objects except for q

--- Learning/C++/practiceProjects/chess.cpp
+++ Learning/C++/practiceProjects/chess.cpp
@@ -12,7 +12,7 @@ string chessBoard[8][8] = {
     {" ___ ", " ___ ", " ___ ", " ___ ", " ___ ", " ___ ", " ___ ", " ___ "},
     {" ___ ", " ___ ", " ___ ", " ___ ", " ___ ", " ___ ", " ___ ", " ___ "},
     {" WP1 ", " WP2 ", " WP3 ", " WP4 ", " WP5 ", " WP6 ", " WP7 ", " WP8 "},
-    {" WR1 ", " WN1 ", " WB1 ", " WQ0 ", " WK0 ", " WB2 ", " WN2 ",  " WR2 "}
+    {" WR1 ", " WN1 ", " WB1 ", " WQ0 ", " WK0 ", " WB2 ", " WN2 ", " WR2 "}
 };
 
 //objects and classes
@@ -23,216 +23,235 @@ class BoardPieces{
         string color;
         int possibleI;
         int possibleJ;
-        void blackMovementCheck(int i, int j){
-            //empty check
-        }
-        void whiteMovementCheck(int i, int j){
-            //empty check
-        }
-        void commonMovementCheck(int i, int j){
-        }
 };
 
 class Pawn : public BoardPieces{
-    public:
-        int possibleI[3];
-        int possibleJ[3];
-        Pawn(string x, string y){
-            pieceName = x;
-            color = y;
-        }
-        void blackMovementCheck(int i, int j){
-            if(chessBoard[i+1][j] == " __ "){
-                possibleI[0] = i + 1;
-                possibleJ[0] = j;
-            }
-            if(chessBoard[i+1][j+1] != " __ "){
-                possibleI[1] = i+1;
-                possibleJ[1] = j+1;
-            }
-            if(chessBoard[i+1][j-1] != " __ "){
-                possibleI[2] = i+1;
-                possibleJ[2] = j-1;
-            }
-        }
-        void whiteMovementCheck(int i, int j){
-            if(chessBoard[i-1][j] == " __ "){
-                possibleI[0] = i - 1;
-                possibleJ[0] = j;
-            }
-            if(chessBoard[i-1][j+1] != " __ "){
-                possibleI[1] = i-1;
-                possibleJ[1] = j+1;
-            }
-            if(chessBoard[i-1][j-1] != " __ "){
-                possibleI[2] = i-1;
-                possibleJ[2] = j-1;
-            }  
-        }
+  private:
+    void exceptionSetter(int index){
+        possibleI[index] = -1;
+        possibleJ[index] = -1;
+    }
+    void verticalMovement(int i, int j){
+      if(chessBoard[i][j] == " ___ "){
+        possibleI[0] = i;
+        possibleJ[0] = j;
+      }else{
+        exceptionSetter(0);
+      }
+    }
+    void diagonalMovement1(int i, int j){
+      if(chessBoard[i][j] != " ___ " && j < 8){
+        possibleI[1] = i;
+        possibleJ[1] = j;
+      }else{
+        exceptionSetter(1);
+      }
+    }
+    void diagonalMovement2(int i, int j){
+      if(chessBoard[i][j] != " ___ " && j > -1){
+        possibleI[2] = i;
+        possibleJ[2] = j;
+      }else{
+        exceptionSetter(2);
+      }
+    }
+  public:
+    int possibleI[3];
+    int possibleJ[3];
+    Pawn(string a, string b){
+              pieceName = a;
+              color = b;
+          }
+    void blackMovementCheck(int i, int j){
+            verticalMovement(i + 1, j);  
+            diagonalMovement1(i + 1, j + 1);
+            diagonalMovement2(i + 1, j - 1);
+          }
+    void whiteMovementCheck(int i, int j){
+              verticalMovement(i - 1, j);
+              diagonalMovement1(i - 1, j + 1);
+              diagonalMovement2(i - 1, j - 1);
+          }
 };
 
 class Rook  : public BoardPieces{
+    private:
+        int index = 0;
+        bool status;
+        void verticalCheck(int x, int j){
+          if(chessBoard[x][j] == " ___ "){
+            verticalSet(x, j);
+          }else{
+            verticalSet(x, j);
+            status = 0;
+          }
+        }
+        void horizontalCheck(int i, int y){
+          if(chessBoard[i][y] == " ___ "){
+            horizontalSet(i, y);
+          }else{
+            horizontalSet(i, y);
+            status = 0;
+          }
+        }
+        void verticalSet(int x, int j){
+          if(status == 1){
+            possibleI[index] = x;
+            possibleJ[index] = j; 
+          }else{
+            possibleI[index] = -1;
+            possibleJ[index] = -1; 
+          }
+          index++;
+        }
+        void horizontalSet(int i, int y){
+          if(status == 1){
+            possibleI[index] = i;  
+            possibleJ[index] = y; 
+          }else{
+            possibleI[index] = -1;  
+            possibleJ[index] = -1;
+          }
+          index++;
+        }
+
     public:
         int possibleI[14];
         int possibleJ[14];
-        Rook(string x, string y){
-            pieceName = x;
-            color = y;
+        Rook(string a, string b){
+            pieceName = a;
+            color = b;
         }
-        void commonMovementCheck(int i, int j){
-            for(int x = 0; x < 7; x++){
-                if(x != i &&  chessBoard[x][j] == " __ "){
-                    possibleI[x] = x;
-                    possibleJ[x] = j;
-                }
-            }
-            for(int y = 7; y < 14; y++){
-                if(y != j &&  chessBoard[i][y] == " __ "){
-                    possibleI[y] = i;
-                    possibleJ[y] = y;
-                }
-            }
+        void rookMovementCheck(int i, int j){     
+          status = 1;
+          for(int x = i+1; x < 8; x++){  
+            verticalCheck(x, j);
+          }
+          status = 1;
+          for(int x = i-1; x > -1; x--){
+            verticalCheck(x, j);
+          }
+          status = 1;
+          for(int y = j+1; y < 8; y++){
+            horizontalCheck(i, y);
+          }
+          status = 1;
+          for(int y = j-1; y > -1; y--){
+            horizontalCheck(i, y);
+          }
         }
 };
 
 class Knight  : public BoardPieces{
-    public:
-        int possibleI[8];
-        int possibleJ[8];
-        Knight(string x, string y){
-            pieceName = x;
-            color = y;
-        }
-        void commonMovementCheck(int i, int j){
-            if(i+2 >= 0 && i+2 < 8 && j+1 >= 0 && j+1 < 8){
-                possibleI[0] = i + 2;
-                possibleJ[0] = j + 1;
-            }
-            if(i+1 >= 0 && i+1 < 8 && j+2 >= 0 && j+2 < 8){
-                possibleI[1] = i + 1;
-                possibleJ[1] = j + 2;
-            }
-            if(i+2 >= 0 && i+2 < 8 && j-1 >= 0 && j-1 < 8){
-                possibleI[2] = i + 2;
-                possibleJ[2] = j - 1;
-            }
-            if(i+1 >= 0 && i+1 < 8 && j-2 >= 0 && j-2 < 8){
-                possibleI[3] = i + 1;
-                possibleJ[3] = j - 2;
-            }
-            if(i-2 >= 0 && i-2 < 8 && j-1 >= 0 && j-1 < 8){
-                possibleI[4] = i - 2;
-                possibleJ[4] = j - 1;
-            }
-            if(i-1 >= 0 && i-1 < 8 && j-2 >= 0 && j-2 < 8){
-                possibleI[5] = i - 1;
-                possibleJ[5] = j - 2;
-            }
-            if(i-2 >= 0 && i-2 < 8 && j+1 >= 0 && j+1 < 8){
-                possibleI[6] = i - 2;
-                possibleJ[6] = j + 1;
-            }
-            if(i-1 >= 0 && i-1 < 8 && j+2 >= 0 && j+2 < 8){
-                possibleI[7] = i - 1;
-                possibleJ[7] = j + 2;
-            }
-        }
+  private:
+    void compareValue(int i, int j, int index){
+      if(i >= 0 && i < 8 && j >= 0 && j < 8){
+        setValue(i, j, index);
+      }else{
+        setValue(-1, -1, index);
+      }
+    }
+    void setValue(int i, int j, int index){
+      possibleI[index] = i;
+      possibleJ[index] = j;
+    }
+  public:
+    int possibleI[8];
+    int possibleJ[8];
+    Knight(string x, string y){
+        pieceName = x;
+        color = y;
+    }
+    void knightMovementCheck(int i, int j){
+      compareValue(i + 2, j + 1, 0);
+      compareValue(i + 1, j + 2, 1);
+      compareValue(i + 2, j - 1, 2);
+      compareValue(i + 1, j - 2, 3);
+      compareValue(i - 2, j - 1, 4);
+      compareValue(i - 1, j - 2, 5);
+      compareValue(i - 2, j + 1, 6);
+      compareValue(i - 1, j + 2, 7);
+    }
 };
 
 class Bishop  : public BoardPieces{
+    private:
+        int index = 0;
+        bool status;
+        int x;
+        int y;
+        void exceptionSetter(){
+            possibleI[index] = -1;
+            possibleJ[index] = -1;
+        }
+        void reset(int i, int j){
+          status = 1;
+          x = i;
+          y = j;
+        }
+        void checkValues(int x, int y){
+              if(chessBoard[x][y] == " ___ "){
+                setValues(x, y);
+              }else{
+                setValues(x, y);
+                status = 0;
+              }
+        }
+        void setValues(int x, int y){
+          if(status == 1){
+            possibleI[index] = x;
+            possibleJ[index] = y; 
+          }else{
+            exceptionSetter();
+          }
+          index++;
+        }
+
     public:
-         int possibleI[13];
+        int possibleI[13];
         int possibleJ[13];
-        Bishop(string x, string y){
-            pieceName = x;
-            color = y;
+        Bishop(string a, string b){
+            pieceName = a;
+            color = b;
         }
-        void commonMovementCheck(int i, int j){
-            int index = 0;
-            for(int x = i; i > 0; i--){
-                for(int y = j; j > 0; j--){
-                    possibleI[index] = x;
-                    possibleJ[index] = y;
-                    index++;
-                }
+        void bishopMovementCheck(int i, int j){
+          reset(i, j);
+          while(x > 0 && y > 0){
+            checkValues(x - 1, y - 1);
+            x--;
+            y--;
+          }
+          reset(i, j);
+          while(x > 0 && y < 7){
+            checkValues(x - 1, y + 1);
+            x--;
+            y++;
+          }
+          reset(i, j);
+          while(x < 7 && y > 0){
+            checkValues(x + 1, y - 1);
+            x++;
+            y--;
+          }
+          reset(i, j);
+          while(x < 7 && y > 0){
+            checkValues(x + 1, x + 1);
+            x++;
+            x--;
+          }
+          if(index != 12){
+            for(int a = index; a < 13; a++){
+                exceptionSetter();
             }
-            for(int x = i; i > 0; i--){
-                for(int y = j; j < 7; j++){
-                    possibleI[index] = x;
-                    possibleJ[index] = y;
-                    index++;  
-                }
-            }
-            for(int x = i; i < 7; i++){
-                for(int y = j; j > 0; j--){
-                    possibleI[index] = x;
-                    possibleJ[index] = y;
-                    index++;
-                }
-            }
-            for(int x = i; i < 7; i++){
-                for(int y = j; j < 7; j++){
-                    possibleI[index] = x;
-                    possibleJ[index] = y;
-                    index++;
-                }
-            }
+          }
+        
         }
+
 };
 
 class Queen  : public BoardPieces{
     public:
-        int possibleI[27];
-        int possibleJ[27];
-        int index = 0;
-        Queen(string x, string y){
-            pieceName = x;
-            color = y;
-        }
-        void commonMovementCheck(int i, int j){
-            //rook movement
-            for(int x = 0; x < 7; x++){
-                if(x != i &&  chessBoard[x][j] == " __ "){
-                    possibleI[x] = x;
-                    possibleJ[x] = j;
-                }
-            }
-            for(int y = 7; y < 14; y++){
-                if(y != j &&  chessBoard[i][y] == " __ "){
-                    possibleI[y] = i;
-                    possibleJ[y] = y;
-                }
-            }
-            //bishop movement
-            for(int x = i; i > 0; i--){
-                for(int y = j; j > 0; j--){
-                    possibleI[index] = x;
-                    possibleJ[index] = y;
-                    index++;
-                }
-            }
-            for(int x = i; i > 0; i--){
-                for(int y = j; j < 7; j++){
-                    possibleI[index] = x;
-                    possibleJ[index] = y;
-                    index++;  
-                }
-            }
-            for(int x = i; i < 7; i++){
-                for(int y = j; j > 0; j--){
-                    possibleI[index] = x;
-                    possibleJ[index] = y;
-                    index++;
-                }
-            }
-            for(int x = i; i < 7; i++){
-                for(int y = j; j < 7; j++){
-                    possibleI[index] = x;
-                    possibleJ[index] = y;
-                    index++;
-                }
-            }
-        }
+
 };
 
 class King  : public BoardPieces{
@@ -298,18 +317,18 @@ struct {
 }boardStructure;
 
 //decleration of functions
-void captureCheck();
+void terminalCheck(string terminal, string initial);
 void whiteTurn();
 void blackTurn();
 void printWhiteBoard();
 void printBlackBoard();
-void declarePieces();
-string kingCheck(string kingName);
+void initialCheck(string coordinate);
+bool kingCheck(string kingName);
 
 //main function
 int main(){
-    printWhiteBoard();
-    printBlackBoard();
+    cout << "Starting the game... \n";
+    whiteTurn();
     return 0;
 }
 
@@ -346,14 +365,58 @@ void printBlackBoard(){
     cout << "    H    G    F    E    D    C    B    A\n";
 }
 
-string kingCheck(string kingName){
-    string status = "Not Alive";
+bool kingCheck(string kingName){
+    bool alive = 0;
     for(int i = 0; i > 8; i++){
         for(int j = 0; j > 8; j++){
             if(chessBoard[i][j] == kingName){
-                status = "Alive";
+                alive = 1;
             }
         }
     }
-    return status;
+    return alive;
 }
+
+void whiteTurn(){
+    string initialCoordinate;
+    string terminalCoordinate;
+    cout << "It is white's turn!\n";
+    printWhiteBoard();
+    cout << "Please enter the coordinates of piece you want to move...\n";
+    cin >> initialCoordinate;
+    initialCheck(initialCoordinate);
+    cout << "You are about to move the piece..." << "" << "\n";
+    cout << "Please enter the coordinates of the tile you want to move your piece...\n";
+    cin >> terminalCoordinate;
+    terminalCheck(terminalCoordinate, initialCoordinate);
+    if(kingCheck(" BK0 ") == 1){
+        cout << "You have moved..." << "" << "to the coordinate..." << terminalCoordinate << "\n";
+        printWhiteBoard();
+        blackTurn();
+    }else{
+        cout << "Checkmate, white wins!";
+    }
+
+}
+
+void blackTurn(){
+    string initialCoordinate;
+    string terminalCoordinate;
+    cout << "It is black's turn!\n";
+    printWhiteBoard();
+    cout << "Please enter the coordinates of piece you want to move...\n";
+    cin >> initialCoordinate;
+    initialCheck(initialCoordinate);
+    cout << "You are about to move the piece..." << "" << "\n";
+    cout << "Please enter the coordinates of the tile you want to move your piece...\n";
+    cin >> terminalCoordinate;
+    terminalCheck(terminalCoordinate, initialCoordinate);
+    if(kingCheck(" WK0 ") == 1){
+        cout << "You have moved..." << "" << "to the coordinate..." << terminalCoordinate << "\n";
+        printWhiteBoard();
+        whiteTurn();
+    }else{
+        cout << "Checkmate, black wins!";
+    }
+}
+
