ab594392f891c1c17c7f01d0649679752432ca52
Pranava Ponvinayagan <67753240+PranavaKP@users.noreply.github.com>
2023-05-29 17:14:42 +0000

    finishedCodingChess;DebuggingLeft

--- Learning/C++/practiceProjects/chess.cpp
+++ Learning/C++/practiceProjects/chess.cpp
@@ -18,6 +18,7 @@ string chessBoard[8][8] = {
 //objects and classes
 class Pawn{
   private:
+    //functions used for movement check
     void exceptionSetter(int index){
         possibleI[index] = -1;
         possibleJ[index] = -1;
@@ -47,8 +48,10 @@ class Pawn{
       }
     }
   public:
+    //possible coords
     int possibleI[3];
     int possibleJ[3];
+    //functions to calculate possible coords
     void blackMovementCheck(int i, int j){
             verticalMovement(i + 1, j);  
             diagonalMovement1(i + 1, j + 1);
@@ -63,6 +66,7 @@ class Pawn{
 
 class Rook{
     private:
+        //variables and functions used to calculate possible coords
         int index = 0;
         bool status;
         void verticalCheck(int x, int j){
@@ -103,8 +107,10 @@ class Rook{
         }
 
     public:
+        //possible coords
         int possibleI[14];
         int possibleJ[14];
+        //function to identify possible coords
         void rookMovementCheck(int i, int j){     
           status = 1;
           for(int x = i+1; x < 8; x++){  
@@ -127,6 +133,7 @@ class Rook{
 
 class Knight{
   private:
+    //functions used in the function for finding possible coords
     void compareValue(int i, int j, int index){
       if(i >= 0 && i < 8 && j >= 0 && j < 8){
         setValue(i, j, index);
@@ -139,8 +146,10 @@ class Knight{
       possibleJ[index] = j;
     }
   public:
+    //possible coords
     int possibleI[8];
     int possibleJ[8];
+    //function used to find possible coords
     void knightMovementCheck(int i, int j){
       compareValue(i + 2, j + 1, 0);
       compareValue(i + 1, j + 2, 1);
@@ -155,6 +164,7 @@ class Knight{
 
 class Bishop{
     private:
+        //functions and variables used in func to find possible coords
         int index = 0;
         bool status;
         int x;
@@ -185,10 +195,11 @@ class Bishop{
           }
           index++;
         }
-
     public:
+        //possible coords
         int possibleI[13];
         int possibleJ[13];
+        //func to find possible coords
         void bishopMovementCheck(int i, int j){
           reset(i, j);
           while(x > 0 && y > 0){
@@ -226,6 +237,8 @@ class Bishop{
 
 class Queen{
     public:
+    int possibleI[27];
+    int possibleJ[27];
     void queenMovementCheck(int i, int j){
 
     }
@@ -233,8 +246,10 @@ class Queen{
 
 class King{
     public:
+        //possible coords
         int possibleI[8];
         int possibleJ[8];
+        //func used to find possible coords
         void kingMovementCheck(int i, int j){
             possibleI[0] = i + 1;
             possibleJ[0] = j;
@@ -257,6 +272,7 @@ class King{
 
 //board structures
 struct {
+    //name of the tiles
     string tileName[64] = {
         "A8", "B8", "C8", "D8", "E8", "F8", "G8", "H8",
         "A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7",
@@ -267,6 +283,7 @@ struct {
         "A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2",
         "A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1"
     };
+    //i value for each tile
     int i[64] = {
         0,0,0,0,0,0,0,0,
         1,1,1,1,1,1,1,1,
@@ -277,6 +294,7 @@ struct {
         6,6,6,6,6,6,6,6,
         7,7,7,7,7,7,7,7
     };
+    //j value for each tile
     int j[64] = {
         0,1,2,3,4,5,6,7,
         0,1,2,3,4,5,6,7,
@@ -290,30 +308,47 @@ struct {
 }boardStructure;
 
 struct{
+  //name of all pieces
   string pieceName[32] = {" BP8 ", " BP7 ", " BP6 ", " BP5 ", " BP4 ", " BP3 ", " BP2 ", " BP1 ",
                           " WP1 ", " WP2 ", " WP3 ", " WP4 ", " WP5 ", " WP6 ", " WP7 ", " WP8 ",
                           " BR2 ", " BN2 ", " BB2 ", " BQ0 ", " BK0 ", " BB1 ", " BN1 ", " BR1 "
                           " WR1 ", " WN1 ", " WB1 ", " WQ0 ", " WK0 ", " WB2 ", " WN2 ", " WR2 "};
+  //piece type number classification
   int pieceClassification[32] = {0 , 0 , 0 , 0 , 0 , 0 , 0 , 0,
                                  1 , 1 , 1 , 1 , 1 , 1 , 1 , 1,
                                  2 , 3 , 4 , 5 , 6 , 4 , 3 , 2,
                                  2 , 3 , 4 , 5 , 6 , 4 , 3 , 2,};
+  //piece color classification
   string pieceColor[32] = {"Black", "Black", "Black", "Black", "Black", "Black", "Black", "Black",
                            "White", "White", "White", "White", "White", "White", "White", "White",
                            "Black", "Black", "Black", "Black", "Black", "Black", "Black", "Black",
                            "White", "White", "White", "White", "White", "White", "White", "White"};
 }pieceNameToObject;
 
+//global variables
+string initialCoordinate;
+string terminalCoordinate;
+string pieceName;
+string currentColor;
+string oppColor;
+int objNumberIndex;
+int initialI;
+int initialJ;
+int terminalI;
+int terminalJ;
+
 //decleration of functions
 void whiteTurn();
 void blackTurn();
 void printWhiteBoard();
 void printBlackBoard();
-int initialCheck(string coordinate, string &pieceName, string color);
-void terminalCheck(string terminal, string initial, int objNumberIndex, int &terminalI, int &terminalJ);
+int initialCheck(string &pieceName, string color);
+void terminalCheck();
+void switchCaseCheck(int maxX, int arrayI[], int arrayJ[]);
 void chessCoordsToNumeric(string coords, int &i, int &j);
 bool kingCheck(string kingName);
 void movePiece();
+void captureCheck();
 
 //main function
 int main(){
@@ -368,17 +403,13 @@ bool kingCheck(string kingName){
 }
 
 void whiteTurn(){
-    string initialCoordinate;
-    string terminalCoordinate;
-    string pieceName;
-    int objNumberIndex;
-    int terminalI;
-    int terminalJ;
+    currentColor = "White";
+    oppColor = "Black";
     cout << "It is white's turn!\n";
     printWhiteBoard();
     cout << "Please enter the coordinates of piece you want to move...\n";
     cin >> initialCoordinate;
-    objNumberIndex = initialCheck(initialCoordinate, pieceName, "White");
+    objNumberIndex = initialCheck(pieceName, currentColor);
     if(pieceName == "null"){
       cout << "You have entered the coordinates for an unvalid piece... Please try again!\n";
       whiteTurn();
@@ -386,7 +417,7 @@ void whiteTurn(){
       cout << "You are about to move the piece..." << pieceName << "\n";
       cout << "Please enter the coordinates of the tile you want to move your piece...\n";
       cin >> terminalCoordinate;
-      terminalCheck(terminalCoordinate, initialCoordinate, objNumberIndex, terminalI, terminalJ);
+      terminalCheck();
       if(terminalI == -1 || terminalJ == -1){
         cout << "You have moved your piece to an invalid space...Please try again!";
         whiteTurn();
@@ -405,140 +436,120 @@ void whiteTurn(){
 }
 
 void blackTurn(){
-    string initialCoordinate;
-    string terminalCoordinate;
-    string pieceName;
-    int objNumberIndex;
-    int terminalI;
-    int terminalJ;
-    cout << "It is white's turn!\n";
-    printWhiteBoard();
+    currentColor = "Black";
+    oppColor = "White";
+    cout << "It is black's turn!\n";
+    printBlackBoard();
     cout << "Please enter the coordinates of piece you want to move...\n";
     cin >> initialCoordinate;
-    objNumberIndex = initialCheck(initialCoordinate, pieceName, "White");
+    objNumberIndex = initialCheck(pieceName, currentColor);
     if(pieceName == "null"){
       cout << "You have entered the coordinates for an unvalid piece... Please try again!\n";
-      whiteTurn();
+      blackTurn();
     }else{
       cout << "You are about to move the piece..." << pieceName << "\n";
       cout << "Please enter the coordinates of the tile you want to move your piece...\n";
       cin >> terminalCoordinate;
-      terminalCheck(terminalCoordinate, initialCoordinate, objNumberIndex, terminalI, terminalJ);
+      terminalCheck();
       if(terminalI == -1 || terminalJ == -1){
         cout << "You have moved your piece to an invalid space...Please try again!";
-        whiteTurn();
+        blackTurn();
       }else{
         movePiece();
-        if(kingCheck(" BK0 ") == 1){
+        if(kingCheck(" WK0 ") == 1){
             cout << "You have moved..." << "" << "to the coordinate..." << terminalCoordinate << "\n";
-            printWhiteBoard();
-            blackTurn();
+            printBlackBoard();
+            whiteTurn();
         }else{
-            cout << "Checkmate, white wins!";
+            cout << "Checkmate, black wins!";
         }
       }
 
     }
 }
 
-int initialCheck(string coordinate, string &pieceName, string color){
-  int i;
-  int j;
-  chessCoordsToNumeric(coordinate, i, j);
-  if(chessBoard[i][j] != " ___ "){
+int initialCheck(string &pieceName, string color){
+  chessCoordsToNumeric(initialCoordinate, initialI, initialJ);
+  if(chessBoard[initialI][initialJ] != " ___ "){
     for(int x = 0; x < 32; x++){
       if(pieceNameToObject.pieceName[x] == pieceName && pieceNameToObject.pieceColor[x] == color){
-        pieceName = chessBoard[i][j];
+        pieceName = chessBoard[initialI][initialJ];
         return x;
       }else{
         pieceName = "null";
         return -1;
       }
     }
+  }
+  pieceName = "null";
+  return -1;
+}
+
+void terminalCheck(){
+  int objNumber;
+  chessCoordsToNumeric(terminalCoordinate, terminalI, terminalJ);
+  objNumber = pieceNameToObject.pieceClassification[objNumberIndex];
+  if(objNumber == 0){
+    Pawn BlackPawn;
+    BlackPawn.blackMovementCheck(initialI, initialJ);
+    switchCaseCheck(3, BlackPawn.possibleI, BlackPawn.possibleJ);
+  }else if(objNumber == 1){
+    Pawn WhitePawn;
+    WhitePawn.whiteMovementCheck(initialI, initialJ);
+    switchCaseCheck(3, WhitePawn.possibleI, WhitePawn.possibleJ);
+  }else if(objNumber == 2){
+    Rook RookPiece;
+    RookPiece.rookMovementCheck(initialI, initialJ);
+    switchCaseCheck(14, RookPiece.possibleI, RookPiece.possibleJ);
+  }else if(objNumber == 3){
+    Knight KnightPiece;
+    KnightPiece.knightMovementCheck(initialI, initialJ);
+    switchCaseCheck(8, KnightPiece.possibleI, KnightPiece.possibleJ);
+  }else if(objNumber == 4){
+    Bishop BishopPiece;
+    BishopPiece.bishopMovementCheck(initialI, initialJ);
+    switchCaseCheck(13, BishopPiece.possibleI, BishopPiece.possibleJ);
+  }else if(objNumber == 5){
+    Queen QueenPiece;
+    QueenPiece.queenMovementCheck(initialI, initialJ);
+    switchCaseCheck(27, QueenPiece.possibleI, QueenPiece.possibleJ);
+  }else if(objNumber == 6){
+    King KingPiece;
+    KingPiece.kingMovementCheck(initialI, initialJ);
+    switchCaseCheck(8, KingPiece.possibleI, KingPiece.possibleJ);
   }else{
-    pieceName = "null";
-    return -1;
+    terminalI = -1;
+    terminalJ = -1;
+  }
+}
+
+void switchCaseCheck(int maxX, int arrayI[], int arrayJ[]){
+  for(int x = 0; x < maxX; x++){
+    if(arrayI[x] == terminalI && arrayJ[x] == terminalJ){
+      captureCheck();
+      break;
+    }
+  }
+}
+
+void captureCheck(){
+  string terminalTileName = chessBoard[terminalI][terminalJ];
+  if(terminalTileName != " ___ "){
+    for(int x = 0; x < 32; x++){
+      if(pieceNameToObject.pieceName[x] == terminalTileName){
+        if(pieceNameToObject.pieceColor[x] != oppColor){
+          terminalI = -1;
+          terminalJ = -1;
+        }
+      }
+    }
   }
 
 }
 
-void terminalCheck(string terminal, string intitial, int objNumberIndex, int &terminalI, int &terminalJ){
-  int initialI;
-  int initialJ;
-  int objNumber;
-  chessCoordsToNumeric(intitial, initialI, initialJ);
-  chessCoordsToNumeric(terminal, terminalI, terminalJ);
-  objNumber = pieceNameToObject.pieceClassification[objNumberIndex];
-  switch (objNumber)
-  {
-    case 0:
-      Pawn BlackPawn;
-      BlackPawn.blackMovementCheck(initialI, initialJ);
-      for(int x = 0; x < 3; x++){
-        if(BlackPawn.possibleI[x] == terminalI && BlackPawn.possibleJ[x] == terminalJ){
-          break;
-        }
-      }
-      break;
-    case 1:
-      Pawn WhitePawn;
-      WhitePawn.whiteMovementCheck(initialI, initialJ);
-      for(int x = 0; x < 3; x++){
-        if(WhitePawn.possibleI[x] == terminalI && WhitePawn.possibleJ[x] == terminalJ){
-          break;
-        }
-      }
-      break;
-    case 2:
-      Rook Rook;
-      Rook.rookMovementCheck(initialI, initialJ);
-      for(int x = 0; x < 14; x++){
-        if(Rook.possibleI[x] == terminalI && Rook.possibleJ[x] == terminalJ){
-          break;
-        }
-      }
-      break;
-    case 3:
-      Knight Knight;
-      Knight.knightMovementCheck(initialI, initialJ);
-      for(int x = 0; x < 8; x++){
-        if(Knight.possibleI[x] == terminalI && Knight.possibleJ[x] == terminalJ){
-          break;
-        }
-      }
-      break;
-    case 4:
-      Bishop Bishop;
-      Bishop.bishopMovementCheck(initialI, initialJ);
-      for(int x = 0; x < 13; x++){
-        if(Knight.possibleI[x] == terminalI && Knight.possibleJ[x] == terminalJ){
-          break;
-        }
-      }
-      break;
-    case 5:
-      Queen Queen;
-      Queen.queenMovementCheck(initialI, initialJ);
-      for(int x = 0; x < 27; x++){
-        if(Knight.possibleI[x] == terminalI && Knight.possibleJ[x] == terminalJ){
-          break;
-        }
-      }
-      break;
-    case 6:
-      King King;
-      King.kingMovementCheck(initialI, initialJ);
-      for(int x = 0; x < 8; x++){
-        if(Knight.possibleI[x] == terminalI && Knight.possibleJ[x] == terminalJ){
-          break;
-        }
-      }
-      break;
-    default:
-      terminalI = -1;
-      terminalJ = -1;
-  }
-
+void movePiece(){
+  chessBoard[initialI][initialJ] = " ___ ";
+  chessBoard[terminalI][terminalJ] = pieceName;
 }
 
 void chessCoordsToNumeric(string coords, int &i, int &j){
